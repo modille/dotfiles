e55940b410c194e5527fb2bcb0d92a85593e0919
Matt Odille <matt.odille@gmail.com>
2021-11-02 10:59:28 -0400

    Remove unused js debugger config

--- config/nvim/lua/modille/debuggers.lua
+++ config/nvim/lua/modille/debuggers.lua
@@ -9,19 +9,7 @@ dap.adapters.node2 = {
   command = 'node',
   args = { os.getenv('HOME') .. '/git/github.com/microsoft/vscode-node-debug2/out/src/nodeDebug.js' },
 }
-dap.configurations.javascript = {
-  {
-    type = 'node2',
-    request = 'launch',
-    cwd = vim.fn.getcwd(),
-    cmd = "node_modules/.bin/jest -t '$result' -- $file",
-    sourceMaps = true,
-    protocol = 'inspector',
-    skipFiles = {'<node_internals>/**/*.js'},
-    console = 'integratedTerminal',
-    port = 9229,
-  },
-}
+
 vim.fn.sign_define('DapBreakpoint', {text='⏸ ', texthl='', linehl='', numhl=''})
 
 -- https://github.com/rcarriga/nvim-dap-ui
