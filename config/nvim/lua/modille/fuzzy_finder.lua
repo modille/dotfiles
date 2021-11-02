25b0dd145388b026d4988bc2921bf2537e4f310b
Matt Odille <matt.odille@gmail.com>
2021-11-02 11:00:04 -0400

    Improve telescope config

--- config/nvim/lua/modille/fuzzy_finder.lua
+++ config/nvim/lua/modille/fuzzy_finder.lua
@@ -9,10 +9,16 @@ require('telescope').setup {
     entry_prefix = '  ',
     color_devicons = true,
     set_env = { ['COLORTERM'] = 'truecolor' }, -- default = nil
+    file_ignore_patterns = {
+      '%.har$',
+    },
     mappings = {
       i = {
+        ['<esc>'] = actions.close,
         ['<C-x>'] = false,
         ['<C-q>'] = actions.send_to_qflist,
+        ['<C-p>'] = actions.cycle_history_prev,
+        ['<C-n>'] = actions.cycle_history_next,
       },
     }
   },
