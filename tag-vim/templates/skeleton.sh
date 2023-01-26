382599f69b244d6ece366b784ced8414e7de6e66
Matt Odille <matt.odille@gmail.com>
2023-01-25 22:39:50 -0500

    Improve shell script template

--- tag-vim/templates/skeleton.sh
+++ tag-vim/templates/skeleton.sh
@@ -1,1 +1,27 @@
 #!/usr/bin/env bash
+
+# Template from: https://sharats.me/posts/shell-script-best-practices/
+
+set -o errexit
+set -o nounset
+set -o pipefail
+if [[ "${TRACE-0}" == "1" ]]; then
+    set -o xtrace
+fi
+
+if [[ "${1-}" =~ ^-*h(elp)?$ ]]; then
+    echo 'Usage: ./script.sh arg-one arg-two
+
+This is an awesome bash script to make your life better.
+
+'
+    exit
+fi
+
+cd "$(dirname "$0")"
+
+main() {
+    echo "do awesome stuff"
+}
+
+main "$@"
