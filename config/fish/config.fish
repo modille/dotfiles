bc56832f5a13d7fe17a32ce68d12dd62bad5de79
Matt Odille <matt.odille@gmail.com>
2023-02-03 10:07:11 -0500

    Upgrade fish to 3.6.0
    See https://fishshell.com/docs/current/relnotes.html#fish-3-6-0-released-january-7-2023

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,5 +1,22 @@
 if status is-interactive
   fish_vi_key_bindings
+
+  abbr --add gco "git checkout"
+  abbr --add gd "git diff"
+  abbr --add gdc "git diff --cached"
+  abbr --add gf "git fetch"
+  abbr --add gfp "git fetch --prune"
+  abbr --add gpr "git pull --rebase"
+  abbr --add gpu "git push --set-upstream origin"
+  abbr --add grh "git reset --hard HEAD^"
+  abbr --add grs "git reset --soft HEAD^"
+  abbr --add gs "git status"
+  abbr --add gsi "git status --ignored"
+  abbr --add gsuir "git submodule update --init --recursive"
+  abbr --add kc "kubectl"
+  abbr --add kcd "kubectl --context\x3ddev"
+  abbr --add kcp "kubectl --context\x3dprd"
+  abbr --add kcs "kubectl --context\x3dstg"
 end
 
 source /opt/homebrew/opt/asdf/libexec/asdf.fish
