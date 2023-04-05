8bd5932555a5c34692f31add87d2385983e2d907
Matt Odille <matt.odille@gmail.com>
2023-04-04 23:54:35 -0400

    Auto-switch more themes based on system theme changes

--- config/fish/functions/change_background.fish
+++ config/fish/functions/change_background.fish
@@ -23,43 +23,22 @@ function change_background --argument mode_setting
     end
   end
 
-  # Neovim
-  # https://github.com/chmln/nvim-ctrl
-  switch $mode
-    case dark
-      nvim-ctrl 'lua require\'modille/colorscheme_dark\'.load();require\'modille/status_line_dark\'.load()'
-    case light
-      nvim-ctrl 'lua require\'modille/colorscheme_light\'.load();require\'modille/status_line_light\'.load()'
+  # Tracks the current mode in a file, and only proceed if the new mode is different than the current mode
+  set -l previous_mode (cat /Users/modille/.local/state/modille/current_background_mode.txt) > /dev/null
+  if test $mode = $previous_mode
+    return
   end
+  echo "$mode" > /Users/modille/.local/state/modille/current_background_mode.txt
+
+  # Neovim
+  # Use Signal SIGWINCH autocmd via https://github.com/will/bgwinch.nvim (seen at https://gitlab.com/gnachman/iterm2/-/issues/7943)
+  # Includes debounce, which was recommended by justinmk https://github.com/neovim/neovim/pull/18029#issuecomment-1094643497
+  # 2023-04-04: iTerm2 was sending SIGWINCH to all running processes, but it seemed to have broken, so send it manually to nvim for now:
+  pkill -WINCH "nvim"
 
   # iTerm2
   # Supported in 3.5.0
   # https://gitlab.com/gnachman/iterm2/-/issues/7943
-
-  # Wallpaper
-  # https://apple.stackexchange.com/questions/40644/how-do-i-change-desktop-background-with-a-terminal-command
-  switch $mode
-    case dark
-      osascript -e 'tell application "System Events" to tell every desktop to set picture to "/Users/odillem2/Pictures/dark.png"'
-    case light
-      osascript -e 'tell application "System Events" to tell every desktop to set picture to "/Users/odillem2/Pictures/light.png"'
-  end
-
-  # Google Chrome
-  # WIP: Requires manual intervention for now
-  # TODO: Use native messaging https://developer.chrome.com/docs/apps/nativeMessaging/
-  switch $mode
-    case dark
-      open -a 'Google Chrome' 'https://chrome.google.com/webstore/detail/solarized-dark-refresh-th/jinmjipikfdhmacpjdddmpffflhehcah'
-    case light
-      open -a 'Google Chrome' 'https://chrome.google.com/webstore/detail/solarized-light/kkaomacmcjmeajgdkgbemiigiphgcabj'
-  end
-
-  # Firefox
-  # TODO: Use native messaging https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/Native_messaging
-
-  # Slack
-  # TODO: Built-in sidebar themes can do this, but custom ones can't yet
 
   # bat, delta
   switch $mode
@@ -68,4 +47,31 @@ function change_background --argument mode_setting
     case light
       set -Ux BAT_THEME 'Solarized (light)'
   end
+
+  # Firefox
+  # The Zen Fox extension README mentioned a fork that used native messaging
+  # I applied those changes on top of the latest Zen Fox and it worked
+  # TODO: Right now it just toggles anytime it receives a message. Ideally it should be told "light" or "dark" and only toggle if needed.
+  pkill -USR1 -f "python.+zen_fox_toggle_light_dark.py"
+
+  # Google Chrome
+  # TODO: Use native messaging https://developer.chrome.com/docs/apps/nativeMessaging/
+  switch $mode
+    case dark
+      open -a 'Google Chrome' 'https://chrome.google.com/webstore/detail/solarized-dark-refresh-th/jinmjipikfdhmacpjdddmpffflhehcah'
+    case light
+      open -a 'Google Chrome' 'https://chrome.google.com/webstore/detail/solarized-light/kkaomacmcjmeajgdkgbemiigiphgcabj'
+  end
+
+  # Wallpaper
+  # TODO: This doesn't work for desktops that have a full-screened app in-focus
+  switch $mode
+    case dark
+      osascript -e 'delay 0.5' -e 'tell application "System Events" to tell every desktop to set picture to "/Users/modille/Pictures/dark.png"'
+    case light
+      osascript -e 'delay 0.5' -e 'tell application "System Events" to tell every desktop to set picture to "/Users/modille/Pictures/light.png"'
+  end
+
+  # Slack
+  # TODO: Built-in sidebar themes can do this, but custom ones can't yet
 end
