303cbb63b5e25cd09620fc3c0f1660b267624018
Matt Odille <matt.odille@gmail.com>
2023-09-13 14:12:14 -0400

    Change fish cursor depending on vim mode

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,5 +1,8 @@
 if status is-interactive
   fish_vi_key_bindings
+  set fish_cursor_default block
+  set fish_cursor_insert line
+  set fish_cursor_replace_one underscore
 
   abbr --add gco "git checkout"
   abbr --add gd "git diff"
