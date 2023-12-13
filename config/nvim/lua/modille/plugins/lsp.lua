4bb2eb4571386635638d6459903b5c6837b60e54
Matt Odille <matt.odille@gmail.com>
2023-12-13 10:45:36 -0500

    Set default keymaps for trouble.nvim

--- config/nvim/lua/modille/plugins/lsp.lua
+++ config/nvim/lua/modille/plugins/lsp.lua
@@ -22,7 +22,16 @@ return {
         },
       },
       'hrsh7th/cmp-nvim-lsp',
-      'folke/trouble.nvim',
+      {
+        'folke/trouble.nvim',
+        keys = {
+          { '<leader>xx', '<cmd>TroubleToggle<cr>' },
+          { '<leader>xw', '<cmd>TroubleToggle workspace_diagnostics<cr>' },
+          { '<leader>xd', '<cmd>TroubleToggle document_diagnostics<cr>' },
+          { '<leader>xq', '<cmd>TroubleToggle quickfix<cr>' },
+          { '<leader>xl', '<cmd>TroubleToggle loclist<cr>' },
+        },
+      },
       {
         'j-hui/fidget.nvim',
         tag = 'legacy',
