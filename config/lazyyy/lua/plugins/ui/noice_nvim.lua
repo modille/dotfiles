b141cd9a6419a92e819743a8bb3813cb1fc6adc4
Matt Odille <matt.odille@gmail.com>
2024-04-17 09:22:23 -0400

    Customize noice

---
+++ config/lazyyy/lua/plugins/ui/noice_nvim.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "folke/noice.nvim",
+    opts = {
+      messages = {
+        view = "mini", -- default "notify"
+        view_error = "split", -- default "notify"
+        view_warn = "mini", -- default "notify"
+        view_history = "messages", -- view for :messages
+        view_search = "virtualtext", -- view for search count messages. Set to `false` to disable
+      },
+    },
+  },
+}
