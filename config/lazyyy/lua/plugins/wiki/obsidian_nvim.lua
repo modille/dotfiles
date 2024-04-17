66b0d31736eb950a20bafa1f4d03daf6a7b6d840
Matt Odille <matt.odille@gmail.com>
2024-04-17 09:18:46 -0400

    Include note title in filename

--- config/lazyyy/lua/plugins/wiki/obsidian_nvim.lua
+++ config/lazyyy/lua/plugins/wiki/obsidian_nvim.lua
@@ -3,7 +3,6 @@ return {
     "epwalsh/obsidian.nvim",
     version = "*", -- recommended, use latest release instead of latest commit
     lazy = true,
-    ft = "markdown",
     dependencies = {
       "nvim-lua/plenary.nvim",
     },
@@ -24,6 +23,25 @@ return {
       daily_notes = {
         template = "daily.md",
       },
+      -- Optional, customize how note IDs are generated given an optional title.
+      ---@param title string|?
+      ---@return string
+      note_id_func = function(title)
+        -- Create note IDs in a Zettelkasten format with a timestamp and a suffix.
+        -- In this case a note with the title 'My new note' will be given an ID that looks
+        -- like '1657296016-my-new-note', and therefore the file name '1657296016-my-new-note.md'
+        local suffix = ""
+        if title ~= nil then
+          -- If title is given, transform it into valid file name.
+          suffix = title:gsub(" ", "-"):gsub("[^A-Za-z0-9-]", ""):lower()
+        else
+          -- If title is nil, just add 4 random uppercase letters to the suffix.
+          for _ = 1, 4 do
+            suffix = suffix .. string.char(math.random(65, 90))
+          end
+        end
+        return tostring(os.time()) .. "-" .. suffix
+      end,
     },
     keys = {
       { "<leader>w<leader>w", "<cmd>ObsidianToday<cr>" },
