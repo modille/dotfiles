083fbd2be52c06fb841583ea8bbd71545d8abba7
Matt Odille <matt.odille@gmail.com>
2024-05-01 11:41:20 -0400

    Use fat headlines

--- config/lazyyy/lua/plugins/lang/markdown/headlines_nvim.lua
+++ config/lazyyy/lua/plugins/lang/markdown/headlines_nvim.lua
@@ -3,6 +3,8 @@ return {
     "lukas-reineke/headlines.nvim",
     opts = {
       markdown = {
+        fat_headlines = true,
+        fat_headline_upper_string = "▃",
         fat_headline_lower_string = "▀", -- https://github.com/lukas-reineke/headlines.nvim/issues/61
       },
     },
