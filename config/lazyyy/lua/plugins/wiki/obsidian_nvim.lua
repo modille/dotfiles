0d4759b9c934995b485b3422c360a0aa011d9fac
Matt Odille <matt.odille@gmail.com>
2024-11-27 09:18:28 -0500

    Fix default obsidian mappings

--- config/lazyyy/lua/plugins/wiki/obsidian_nvim.lua
+++ config/lazyyy/lua/plugins/wiki/obsidian_nvim.lua
@@ -23,6 +23,29 @@ return {
       daily_notes = {
         template = "daily.md",
       },
+      mappings = {
+        -- Overrides the 'gf' mapping to work on markdown/wiki links within your vault.
+        ["gf"] = {
+          action = function()
+            return require("obsidian").util.gf_passthrough()
+          end,
+          opts = { noremap = false, expr = true, buffer = true },
+        },
+        -- Toggle check-boxes.
+        ["<leader>ch"] = {
+          action = function()
+            return require("obsidian").util.toggle_checkbox()
+          end,
+          opts = { buffer = true },
+        },
+        -- Smart action depending on context, either follow link or toggle checkbox.
+        ["<cr>"] = {
+          action = function()
+            return require("obsidian").util.smart_action()
+          end,
+          opts = { buffer = true, expr = true },
+        },
+      },
       -- Optional, customize how note IDs are generated given an optional title.
       ---@param title string|?
       ---@return string
