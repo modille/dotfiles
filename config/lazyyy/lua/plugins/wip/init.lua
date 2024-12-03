acf210faed89983bc1708ed67a412dd29ea955aa
Matt Odille <matt.odille@gmail.com>
2024-12-03 00:36:23 -0500

    Try treesj for toggling ruby blocks

--- config/lazyyy/lua/plugins/wip/init.lua
+++ config/lazyyy/lua/plugins/wip/init.lua
@@ -31,4 +31,27 @@ return {
     "stevearc/overseer.nvim",
     opts = {},
   },
+
+  {
+    "Wansmer/treesj",
+    -- keys = { "<space>m", "<space>j", "<space>s" },
+    dependencies = { "nvim-treesitter/nvim-treesitter" },
+    config = function()
+      require("treesj").setup({
+        ---@type boolean Use default keymaps (<space>m - toggle, <space>j - join, <space>s - split)
+        use_default_keymaps = false,
+      })
+    end,
+    keys = {
+      {
+        "<space>bt",
+        function()
+          require("treesj").toggle()
+        end,
+        desc = "Toggle treesj",
+      },
+      -- { "<space>j", function() require("treesj").join() end, desc = "Join treesj" },
+      -- { "<space>s", function() require("treesj").split() end, desc = "Split treesj" },
+    },
+  },
 }
