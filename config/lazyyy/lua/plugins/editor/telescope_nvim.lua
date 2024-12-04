b16e036d9f7faf265afda86e8033b65227e807b0
Matt Odille <matt.odille@gmail.com>
2024-12-04 17:50:53 -0500

    Open telescope selections in current split

--- config/lazyyy/lua/plugins/editor/telescope_nvim.lua
+++ config/lazyyy/lua/plugins/editor/telescope_nvim.lua
@@ -2,6 +2,10 @@ return {
   {
     "nvim-telescope/telescope.nvim",
     opts = function(_, opts)
+      opts.defaults.get_selection_window = function()
+        return 0
+      end
+
       -- On macOS, M-q enters "œ" and it's hard to disable that, so use something else for the default M-q behavior
       local actions = require("telescope.actions")
       opts.defaults.mappings.i = vim.tbl_extend("force", opts.defaults.mappings.i, {
