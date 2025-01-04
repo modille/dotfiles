b88dedb5228499fe0fa569115de316f35740463f
Matt Odille <matt.odille@gmail.com>
2025-01-03 19:49:18 -0500

    Remove unused GitLab domain

--- config/lazyyy/lua/plugins/coding/vim-fugitive.lua
+++ config/lazyyy/lua/plugins/coding/vim-fugitive.lua
@@ -4,7 +4,6 @@ return {
     lazy = false, -- for "review" git alias
     dependencies = { "shumphrey/fugitive-gitlab.vim" },
     init = function()
-      vim.g.fugitive_gitlab_domains = { "https://gitlab.upmc.com", "https://gitlab.com" }
       vim.cmd([[ command! -nargs=1 Browse silent execute '!open' shellescape(<q-args>,1) ]]) -- fix :GBrowse when netrw isn't loaded
     end,
     keys = {
