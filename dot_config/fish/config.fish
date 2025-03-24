dc739ef81beb81bf379310875e277e3143ecce65
Matt Odille <matt.odille@gmail.com>
2025-03-24 09:58:54 -0400

    Configure libpq

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -68,6 +68,17 @@ if test -d /opt/homebrew/opt/sqlite
     # For pkg-config to find sqlite you may need to set:
     set -gx PKG_CONFIG_PATH /opt/homebrew/opt/sqlite/lib/pkgconfig
 end
+if test -d /opt/homebrew/opt/libpq
+    # If you need to have libpq first in your PATH, run:
+    fish_add_path /opt/homebrew/opt/libpq/bin
+
+    # For compilers to find libpq you may need to set:
+    set -gx LDFLAGS -L/opt/homebrew/opt/libpq/lib
+    set -gx CPPFLAGS -I/opt/homebrew/opt/libpq/include
+
+    # For pkg-config to find libpq you may need to set:
+    set -gx PKG_CONFIG_PATH /opt/homebrew/opt/libpq/lib/pkgconfig
+end
 
 # ASDF configuration code
 if test -z $ASDF_DATA_DIR
